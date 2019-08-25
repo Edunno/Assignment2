fb4065104f9b58f106af5365f41bab67444e1203
Edunno <tamericandream@hotmail.com>
2019-08-25 21:35:40 +0200

    Car and FPS start

---
+++ Assignment2/Assets/CamFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollow : MonoBehaviour
+{   
+    public Camera fpsCam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /* this.transform.localRotation = fpsCam.transform.rotation.normalized;
+        Vector3 dVect = fpsCam.transform.forward;
+        this.transform.localPosition = dVect;*/
+        Vector3 pos = fpsCam.WorldToViewportPoint (transform.position);
+         pos.x = Mathf.Clamp01(pos.x);
+         pos.y = Mathf.Clamp01(pos.y);
+         transform.position = fpsCam.ViewportToWorldPoint(pos);
+    }
+}
---
+++ Assignment2/Assets/fireGun.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fireGun : MonoBehaviour
+{
+    public GameObject flare;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0)){
+            Vector3 flarePos = this.transform.position+this.transform.right.normalized;
+            Instantiate(flare,flarePos,Quaternion.identity);
+        }
+    }
+}
