08aa0074b51dc7bd9357e20e4b5bcd366451ae5a
Edunno <tamericandream@hotmail.com>
2019-08-26 11:09:10 +0200

    Bullet and fire added

--- Assignment2/Assets/fireGun.cs
+++ Assignment2/Assets/fireGun.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class fireGun : MonoBehaviour
 {
     public GameObject flare;
+    public GameObject bullet;
+    public AudioSource ASource;
     // Start is called before the first frame update
     void Start()
     {
+        
         
     }
 
@@ -15,8 +18,15 @@ public class fireGun : MonoBehaviour
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Mouse0)){
+            
             Vector3 flarePos = this.transform.position+this.transform.right.normalized;
-            Instantiate(flare,flarePos,Quaternion.identity);
+            flarePos.y = flarePos.y-(float)0.2;
+            GameObject deployedFlare = (GameObject) Instantiate(flare,flarePos,Quaternion.identity);
+            ASource.Play();
+            Destroy(deployedFlare,1);
+            GameObject tempB = (GameObject) Instantiate(bullet,this.transform.position,Quaternion.identity);
+            tempB.GetComponent<Rigidbody>().AddForce(this.transform.right*200);
+            Destroy(tempB,10);
         }
     }
 }
